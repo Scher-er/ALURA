392e32efec31f573764f282d57b4f9522f415e6b
Scherer <scherer.csch@gmail.com>
2025-03-23 19:22:13 -0300

    Add initial version of "Jogo da Adivinhação" game
    This commit includes the implementation of a simple number guessing game written in C. The program allows the user to guess a secret number with feedback provided for each attempt.

---
+++ Jogo da adivinhação.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(int argc, char const *argv[])
+ {
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    printf("Bem vindo, ao joj!");
+
+ 
+    int nsecrecto = 42;
+ 
+    int chute;
+    int ganhou = 0;
+    int tentativas = 1;
+
+    while(ganhou == 0){
+        printf("Tenativa %d \n", tentativas);
+        printf("Digite o seu chute: ");
+       
+        scanf("%d", &chute);
+        printf("Seu chute foi %d\n", chute);
+        
+        if(chute < 0){
+            printf("Você não pode chutar números negativos!\n");
+            continue;
+        }
+
+        int acertou = (chute == nsecrecto);
+        int maior = chute > nsecrecto;
+
+        if(acertou){
+            printf("Parabéns! Você acertou!\n");
+            printf("Você é um bom jogador!\n");
+            
+            ganhou = 1;
+        }
+        else if(maior){
+            printf("Seu chute foi maior que o número secreto!\n");
+        }
+        else{
+            printf("Seu chute foi menor que o número secreto!\n");
+        }
+
+        tentativas++;
+    }
+    printf("Fim de jogo!\n");
+    return 0;
+}
